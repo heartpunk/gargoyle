dc865ff3b0fe2a321e458d5541825a5d81b15fd7
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-11-28 14:24:56 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gargoyle::Application.config.secret_key_base = '01d74317a89314ad44975de06ad92dd97dd2326b6d3cdbe0aeff25492ecb92f1a011bfbd1001cc419ca8a2f1381a8179a4377be937558b1128ccbe352b82add1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gargoyle::Application.config.session_store :cookie_store, key: '_gargoyle_session'
